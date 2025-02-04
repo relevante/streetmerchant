8df4106663536e44f6b74ba0c4d526e8c77d5567
James Nick Sears <nick@cousins-sears.com>
2025-02-04 14:33:55 -0500

    Added 5090 listing to Amazon

--- src/store/model/amazon.ts
+++ src/store/model/amazon.ts
@@ -979,6 +979,150 @@ export const Amazon: Store = {
       series: 'arc',
       url: 'https://www.amazon.com/dp/B0DPM9923G',
     },
+    {
+      brand: 'asus',
+      cartUrl:
+        'https://www.amazon.com/gp/aws/cart/add.html?ASIN.1=B0DS2WQZ2M&Quantity.1=1',
+      model: 'astral',
+      series: '5090',
+      url: 'https://www.amazon.com/dp/B0DS2WQZ2M',
+    },
+    {
+      brand: 'asus',
+      cartUrl:
+        'https://www.amazon.com/gp/aws/cart/add.html?ASIN.1=B0DS2Z8854&Quantity.1=1',
+      model: 'tuf',
+      series: '5090',
+      url: 'https://www.amazon.com/dp/B0DS2Z8854',
+    },
+    {
+      brand: 'asus',
+      cartUrl:
+        'https://www.amazon.com/gp/aws/cart/add.html?ASIN.1=B0DS2X13PH&Quantity.1=1',
+      model: 'tuf oc',
+      series: '5090',
+      url: 'https://www.amazon.com/dp/B0DS2X13PH',
+    },
+    {
+      brand: 'gigabyte',
+      cartUrl:
+        'https://www.amazon.com/gp/aws/cart/add.html?ASIN.1=B0DT7GHQMD&Quantity.1=1',
+      model: 'aorus master',
+      series: '5090',
+      url: 'https://www.amazon.com/dp/B0DT7GHQMD',
+    },
+    {
+      brand: 'gigabyte',
+      cartUrl:
+        'https://www.amazon.com/gp/aws/cart/add.html?ASIN.1=B0DVCH9WJH&Quantity.1=1',
+      model: 'master ice',
+      series: '5090',
+      url: 'https://www.amazon.com/dp/B0DVCH9WJH',
+    },
+    {
+      brand: 'gigabyte',
+      cartUrl:
+        'https://www.amazon.com/gp/aws/cart/add.html?ASIN.1=B0DT7GBNWQ&Quantity.1=1',
+      model: 'gaming oc',
+      series: '5090',
+      url: 'https://www.amazon.com/dp/B0DT7GBNWQ',
+    },
+    {
+      brand: 'gigabyte',
+      cartUrl:
+        'https://www.amazon.com/gp/aws/cart/add.html?ASIN.1=B0DVCF7XDF&Quantity.1=1',
+      model: 'aorus xtreme waterforce',
+      series: '5090',
+      url: 'https://www.amazon.com/dp/B0DVCF7XDF',
+    },
+    {
+      brand: 'gigabyte',
+      cartUrl:
+        'https://www.amazon.com/gp/aws/cart/add.html?ASIN.1=B0DVCGBPL9&Quantity.1=1',
+      model: 'aorus xtreme waterforce wb',
+      series: '5090',
+      url: 'https://www.amazon.com/dp/B0DVCGBPL9',
+    },
+    {
+      brand: 'gigabyte',
+      cartUrl:
+        'https://www.amazon.com/gp/aws/cart/add.html?ASIN.1=B0DT7GMXHB&Quantity.1=1',
+      model: 'windforce oc',
+      series: '5090',
+      url: 'https://www.amazon.com/dp/B0DT7GMXHB',
+    },
+    {
+      brand: 'msi',
+      cartUrl:
+        'https://www.amazon.com/gp/aws/cart/add.html?ASIN.1=B0DT7JYTXQ&Quantity.1=1',
+      model: 'suprim soc',
+      series: '5090',
+      url: 'https://www.amazon.com/dp/B0DT7JYTXQ',
+    },
+    {
+      brand: 'msi',
+      cartUrl:
+        'https://www.amazon.com/gp/aws/cart/add.html?ASIN.1=B0DT7L98J1&Quantity.1=1',
+      model: 'gaming trio oc',
+      series: '5090',
+      url: 'https://www.amazon.com/dp/B0DT7L98J1',
+    },
+    {
+      brand: 'msi',
+      cartUrl:
+        'https://www.amazon.com/gp/aws/cart/add.html?ASIN.1=B0DT7K9VV3&Quantity.1=1',
+      model: 'suprim liquid soc',
+      series: '5090',
+      url: 'https://www.amazon.com/dp/B0DT7K9VV3',
+    },
+    {
+      brand: 'msi',
+      cartUrl:
+        'https://www.amazon.com/gp/aws/cart/add.html?ASIN.1=B0DT7KGND2&Quantity.1=1',
+      model: 'vanguard soc',
+      series: '5090',
+      url: 'https://www.amazon.com/dp/B0DT7KGND2',
+    },
+    {
+      brand: 'msi',
+      cartUrl:
+        'https://www.amazon.com/gp/aws/cart/add.html?ASIN.1=B0DT7L992Z&Quantity.1=1',
+      model: 'vanguard soc launch edition',
+      series: '5090',
+      url: 'https://www.amazon.com/dp/B0DT7L992Z',
+    },
+    {
+      brand: 'msi',
+      cartUrl:
+        'https://www.amazon.com/gp/aws/cart/add.html?ASIN.1=B0DT7JS6BG&Quantity.1=1',
+      model: 'ventus 3x oc',
+      series: '5090',
+      url: 'https://www.amazon.com/dp/B0DT7JS6BG',
+    },
+    {
+      brand: 'pny',
+      cartUrl:
+        'https://www.amazon.com/gp/aws/cart/add.html?ASIN.1=B0DTJFSSZG&Quantity.1=1',
+      model: 'epic x',
+      series: '5090',
+      url: 'https://www.amazon.com/dp/B0DTJFSSZG',
+    },
+    {
+      brand: 'pny',
+      cartUrl:
+        'https://www.amazon.com/gp/aws/cart/add.html?ASIN.1=B0DTJF8YT4&Quantity.1=1',
+      model: 'oc',
+      series: '5090',
+      url: 'https://www.amazon.com/dp/B0DTJF8YT4',
+    },
+    {
+      brand: 'pny',
+      cartUrl:
+        'https://www.amazon.com/gp/aws/cart/add.html?ASIN.1=B0DTJF8YT4&Quantity.1=1',
+      model: 'oc',
+      series: '5090',
+      url: 'https://www.amazon.com/dp/B0DTJF8YT4',
+    },
   ],
   name: 'amazon',
   country: 'US',
